c4f30873c6d86b3070207f15ba8107b36089b22a
shree1296 <55866094+shree1296@users.noreply.github.com>
2025-05-07 21:14:09 +0530

    Update main.tf

--- snowflake_infrastructure/modules/users_roles/main.tf
+++ snowflake_infrastructure/modules/users_roles/main.tf
@@ -1,40 +1,48 @@
-# Create all custom roles
+###########################################
+# Create Custom Roles in Snowflake
+###########################################
 resource "snowflake_role" "custom_roles" {
   for_each = var.roles
 
-  name    = each.key
-  comment = each.value.comment
+  name    = each.key                       # Role name (e.g., dev_admin)
+  comment = each.value.comment             # Description for the role
 }
 
-# Create Snowflake users
+###########################################
+# Create Users in Snowflake
+###########################################
 resource "snowflake_user" "users" {
   for_each = var.users
 
   name                  = each.key
-  login_name            = each.value.login_name
-  display_name          = each.value.display_name
-  email                 = each.value.email
+  login_name            = each.value.login_name             # Unique login username
+  display_name          = each.value.display_name           # Full name for UI
+  email                 = each.value.email                  # User email address
   first_name            = each.value.first_name
   last_name             = each.value.last_name
-  default_role          = each.value.default_role
-  default_warehouse     = each.value.default_warehouse
-  disabled              = false
-  must_change_password  = true
-  comment               = each.value.comment
+  default_role          = each.value.default_role           # Role assigned at login
+  default_warehouse     = each.value.default_warehouse      # Default WH for user
+  disabled              = false                             # Ensure user is active
+  must_change_password  = true                              # Force password reset at first login
+  comment               = each.value.comment                # Notes about the user
 }
 
-# Grant default role to users
+###########################################
+# Grant Default Role to Each User
+###########################################
 resource "snowflake_role_grants" "user_role_grants" {
   for_each = var.users
 
-  role_name = each.value.default_role
-  users     = [each.key]
+  role_name = each.value.default_role      # Assign default role
+  users     = [each.key]                   # To the specific user
 }
 
-# Role inheritance for RBAC hierarchy
+###########################################
+# Define RBAC Hierarchy (Role Inheritance)
+###########################################
 resource "snowflake_role_grants" "rbac_hierarchy" {
   for_each = var.role_hierarchy
 
-  role_name = each.key                   # Child role
-  roles     = each.value.inherits_from  # List of parent roles
+  role_name = each.key                     # Child role
+  roles     = each.value.inherits_from     # Parent roles (can include SYSADMIN, SECURITYADMIN, etc.)
 }
