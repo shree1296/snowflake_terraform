6842e738419d671a82c43cbdf9218c51df979b4f
shree1296 <55866094+shree1296@users.noreply.github.com>
2025-05-08 12:18:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
 
+provider "snowflake" {
+  account  = var.snowflake_account
+  username = var.snowflake_user
+  password = var.snowflake_password
+  region   = var.snowflake_region
+  role     = "SECURITYADMIN"
+}
