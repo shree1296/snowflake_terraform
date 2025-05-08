a3a483323e0b4c18fc7db3b33c29133b98f805b5
shree1296 <55866094+shree1296@users.noreply.github.com>
2025-05-08 12:19:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
 
+variable "snowflake_account" {}
+variable "snowflake_user" {}
+variable "snowflake_password" {}
+variable "snowflake_region" {}
+
+# Custom roles
+variable "custom_roles" {
+  type    = list(string)
+  default = ["DEV_ADMIN", "QA_ANALYST", "PROD_SUPPORT"]
+}
+
+# Azure AD mapped group names (as roles in Snowflake)
+variable "aad_roles" {
+  type = map(string)
+  default = {
+    DEV_ANALYST   = "AAD_DEV_ANALYST"
+    QA_ENGINEER   = "AAD_QA_ENGINEER"
+    PROD_SUPPORT  = "AAD_PROD_SUPPORT"
+  }
+}
