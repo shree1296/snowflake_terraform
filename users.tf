f2edfb86dcd1d151a929343bada386b4b472ed57
shree1296 <55866094+shree1296@users.noreply.github.com>
2025-05-08 12:33:43 +0530

    Update users.tf

--- users.tf
+++ users.tf
@@ -1,1 +1,12 @@
 
+resource "snowflake_user" "dev_user" {
+  name     = "dev_user"
+  login_name = "dev_user_login"
+  default_warehouse = "DEV_WAREHOUSE"
+  default_role      = "DEV_ROLE"
+  default_namespace = "DEV_DATABASE.SCHEMA"
+
+  # Since you're using Azure SSO, no password is needed
+  authentication = "EXTERNALBROWSER"
+  comment = "Dev user for dev environment"
+}
