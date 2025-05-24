724bcce9025828a5ce9368bc94a5fb189c75e561
shree1296 <55866094+shree1296@users.noreply.github.com>
2025-05-24 14:06:35 +0530

    Delete snowflake_infra directory

--- snowflake_infra/grants.tf
+++
@@ -1,7 +0,0 @@
-# Assign Azure AD groups to Snowflake roles
-resource "snowflake_role_grants" "aad_role_grants" {
-  for_each = var.aad_roles
-
-  role_name = each.key
-  roles     = [each.value]  # Assuming AAD group is already mapped as a Snowflake role
-}
--- snowflake_infra/roles.tf
+++
@@ -1,6 +0,0 @@
-resource "snowflake_role" "custom" {
-  for_each = toset(var.custom_roles)
-
-  name = each.key
-  comment = "Custom role for ${each.key}"
-}
